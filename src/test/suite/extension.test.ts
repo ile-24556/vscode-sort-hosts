6b7ebb3b63692c0bc29261ed565ae73036ffc849
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2022-07-31 13:29:13 +0900

    Remove sample test and comments

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,11 +1,7 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
-
 import { compareHostnames } from '../../sort-hosts';
+
 
 suite('Extension Test Suite', () => {
     vscode.window.showInformationMessage('Start all tests.');
@@ -27,10 +23,5 @@ suite('Extension Test Suite', () => {
             const correctAnswer = pair[1];
             assert.deepStrictEqual(input.sort(compareHostnames), correctAnswer);
         }
-    });
-
-    test('Sample test', () => {
-        assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-        assert.strictEqual(-1, [1, 2, 3].indexOf(0));
     });
 });
